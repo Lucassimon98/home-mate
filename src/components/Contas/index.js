3beb37c6d8698253b4a5de7f05f4e21145b38008
Lucas Henrique Simon <lucassimon98@gmail.com>
2023-02-17 16:02:27 -0300

    Adicionando funções de descrição em cada conta
    Adicionando funções de descrição em cada conta

--- src/components/Contas/index.js
+++ src/components/Contas/index.js
@@ -4,10 +4,10 @@ import { useState, useEffect } from 'react';
 function ContasPagar() {
   const [contas, setContas] = useState([]);
 
-  const adicionarConta = (valor, parcelas) => {
+  const adicionarConta = (valor, parcelas, descricao) => {
     const novasContas = [];
     for (let i = 1; i <= parcelas; i++) {
-      novasContas.push({ valor: valor / parcelas, pago: false });
+      novasContas.push({ valor: valor / parcelas, pago: false, descricao });
     }
     setContas([...contas, ...novasContas]);
   };
@@ -32,14 +32,15 @@ function ContasPagar() {
   }, [contas]);
 
   return (
-    <div className='d-flex flex-column align-items-center'>
+    <div>
       <h2>Contas a pagar</h2>
-      <form className='d-flex flex-column align-items-end'
+      <form
         onSubmit={(event) => {
           event.preventDefault();
           const valor = Number(event.target.elements.valor.value);
-          const parcelas = Number(event.target.elements.parcelas.value);
-          adicionarConta(valor, parcelas);
+          const parcelas = Number(event.target.elements.parcelas.value) || 1;
+          const descricao = event.target.elements.descricao.value;
+          adicionarConta(valor, parcelas, descricao);
           event.target.reset();
         }}
       >
@@ -51,12 +52,18 @@ function ContasPagar() {
           Parcelas:
           <input className='ms-1' type="number" name="parcelas" required />
         </label>
+        <label>
+          Descrição:
+          <input type="text" name="descricao" />
+        </label>
         <button className='w-100 btn-primary' type="submit">Adicionar conta</button>
       </form>
       <table className='w-100'>
         <thead className='w-100'>
           <tr className='d-flex w-100 justify-content-spacearound'>
             <th>Valor</th>
+            <th>Descrição</th>
+            <th>Parcelas</th>
             <th>Pago</th>
             <th>Ações</th>
           </tr>
@@ -65,6 +72,8 @@ function ContasPagar() {
           {contas.map((conta, index) => (
             <tr key={index} className={conta.pago ? 'pago d-flex w-100 justify-content-spacearound' : 'd-flex w-100 justify-content-spacearound'}>
               <td>{conta.valor.toFixed(2)}</td>
+              <td>{conta.descricao || '-'}</td>
+              <td>{conta.parcelas || '1'}</td>
               <td>{conta.pago ? 'Sim' : 'Não'}</td>
               <td>
                 <button className='btn-primary'
