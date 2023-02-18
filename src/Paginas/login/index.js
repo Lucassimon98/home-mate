b9795b02b3e80e5e3abaf2f6caa49265ddaf6c11
Lucas Henrique Simon <lucassimon98@gmail.com>
2023-02-18 11:29:37 -0300

    Update index.js

--- src/Paginas/login/index.js
+++ src/Paginas/login/index.js
@@ -1,15 +1,23 @@
-<button onClick={signInWithGoogle}>Entrar com o Google</button>
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
-// defina a função de login com o provedor de login do Google
-const signInWithGoogle = () => {
-  firebase.auth().signInWithPopup(provider)
-    .then((result) => {
-      // o usuário foi autenticado com sucesso
-      const user = result.user;
-      console.log(user);
-    })
-    .catch((error) => {
-      // ocorreu um erro ao autenticar o usuário
-      console.log(error);
-    });
-};
+function Login() {
+    const signInWithGoogle = () => {
+        firebase.auth().signInWithPopup(provider)
+          .then((result) => {
+            // o usuário foi autenticado com sucesso
+            const user = result.user;
+            console.log(user);
+          })
+          .catch((error) => {
+            // ocorreu um erro ao autenticar o usuário
+            console.log(error);
+          });
+      };
+
+  return (
+    <button onClick={signInWithGoogle}>Entrar com o Google</button>
+  );
+}
+
+export default Login;
